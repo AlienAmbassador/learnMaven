1dff6e904e91125197feafcc12b95a2d2a48a2bf
Lada Biriukova <biruyk.offf@yandex.ru>
2021-10-31 22:13:03 +0300

    Added names

--- src/main/java/ru/learnup/javaqa/learnupmvn/Game/Game.java
+++ src/main/java/ru/learnup/javaqa/learnupmvn/Game/Game.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Game {
 
-    public static boolean isGreenLight = false;
+    public static boolean isGreenLight = true;
     public static int MAX_SPEED = 3;
 
     public static int amountOfLosers(int[] speed) {
@@ -59,10 +59,38 @@ public class Game {
         return winnersSpeed;
     }
 
+
+    public static String[] winnersNames(String[] names) {
+        int cnt = 0;
+        String[] winners = new String[0];
+        for (int i = 0; i < names.length; i++) {
+            if (i % 2 != 0) {
+                if (isGreenLight) {
+                    if (Integer.parseInt(names[i]) > MAX_SPEED) {
+                        winners = Arrays.copyOf(winners, (winners.length + 1));
+                        winners[cnt] = names[i - 1];
+                        cnt++;
+                    }
+                } else {
+                    if (Integer.parseInt(names[i]) <= MAX_SPEED) {
+                        winners = Arrays.copyOf(winners, (winners.length + 1));
+                        winners[cnt] = names[i - 1];
+                        cnt++;
+
+                    }
+                }
+            }
+        }
+        return winners;
+    }
+
+
     public static void main(String[] args) {
+        String[] names = {"Danya", "10", "Kate", "2", "Vlad", "0", "Lada", "100500", "Kirill", "6"};
         int[] speedOfPlayers = {3, 4, 0, 6};
         System.out.println(amountOfLosers(speedOfPlayers));
         System.out.println(Arrays.toString(speedsOfLosers(speedOfPlayers)));
         System.out.println(Arrays.toString(speedsOfWinners(speedOfPlayers)));
+        System.out.println(Arrays.toString(winnersNames(names)));
     }
 }
