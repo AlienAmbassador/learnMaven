53d43003ce9b0b44cd587c4a017ebff012c5e1ab
Lada Biriukova <biruyk.offf@yandex.ru>
2021-11-14 15:39:17 +0300

    hw22

--- src/test/java/ru/learnup/javaqa/learnupmvn/Game/GameTest.java
+++ src/test/java/ru/learnup/javaqa/learnupmvn/Game/GameTest.java
@@ -1,30 +1,33 @@
-//package ru.learnup.javaqa.learnupmvn.Game;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class GameTest {
-//
-//
-//    @Test
-//    public void shouldTellIfLosesInRegularGame() {
-//        Game game = new Game(true);
-//        int speedOfPlayer = 5;
-//
-//        boolean actual = game.isFailed(speedOfPlayer);
-//        boolean expected = false;
-//
-//        Assertions.assertEquals(expected, actual, "Amount Of Losers Incorrect");
-//    }
-//
-//    @Test
-//    public void shouldTellIfLosesInGameWithMaxSpeed() {
-//        SpeedyGame speedyGame = new SpeedyGame(true, 4);
-//        int speedOfPlayer = 5;
-//
-//        boolean actual = speedyGame.isFailed(speedOfPlayer);
-//        boolean expected = false;
-//
-//        Assertions.assertEquals(expected, actual, "Amount Of Losers Incorrect");
-//    }
-//}
+package ru.learnup.javaqa.learnupmvn.Game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+
+    @Test
+    public void shouldTellRoundsInRegularGame() {
+        Game game = new Game(true);
+        GameManager gameManager = new GameManager(game);
+
+        int[] speedOfPlayer = {5, 3, 7, 6, 0};
+
+        int actual = gameManager.roundsCNT(speedOfPlayer);
+        int expected = 4;
+
+        Assertions.assertEquals(expected, actual, "Amount Of Losers Incorrect");
+    }
+
+    @Test
+    public void shouldTellRoundsInGameWithSpeed() {
+        SpeedyGame speedyGame = new SpeedyGame(true, 4);
+        GameManager speedyGameManager = new GameManager(speedyGame);
+        int[] speedOfPlayer = {5, 3, 7, 6, 0};
+
+        int actual = speedyGameManager.roundsCNT(speedOfPlayer);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual, "Amount Of Losers Incorrect");
+    }
+}
